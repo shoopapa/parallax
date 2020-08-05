5cd76a937464dc1bef66e87564a7403135ef0172
jdavis <jdavis@galileosuite.com>
2020-08-05 11:43:03 -0400

    Updates

---
+++ precache-manifest.16420cfcdc08caa4c2922c35ca0cf64e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e30663c52a32ce0770db8fc8baa40f5",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9607d34bf987d49518b7",
+    "url": "/react-gh-pages/static/css/main.1e6a270a.chunk.css"
+  },
+  {
+    "revision": "02f864340a1ed427a592",
+    "url": "/react-gh-pages/static/js/2.43c7d409.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.43c7d409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9607d34bf987d49518b7",
+    "url": "/react-gh-pages/static/js/main.6ab057a8.chunk.js"
+  },
+  {
+    "revision": "c0ba012aa3591bc49409",
+    "url": "/react-gh-pages/static/js/runtime-main.d6ecfbe3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
