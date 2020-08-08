66509068945259b47ff9cfe3b3bb54ae401b086f
jdavis <jdavis@galileosuite.com>
2020-08-07 21:36:23 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/parallax/precache-manifest.284afeac5c5882cea65782242f0a1f50.js"
+  "/parallax/precache-manifest.6cd762e3f0b73fa054de09736f65d9a9.js"
 );
 
 self.addEventListener('message', (event) => {
